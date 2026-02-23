ef4bf1f2559f5202296a7bfaf4016f0546b72d49
shaunmartinx <shaunmartinxj@gmail.com>
2026-02-23 11:35:46 +0530

    Add files via upload

---
+++ OOPSBannerApp03.java
@@ -0,0 +1,48 @@
+public class UC3_OOPS_StringJoin {
+    public static void main(String[] args) {
+
+     
+
+        System.out.println(String.join(" ",
+                " ***** ",
+                " ***** ",
+                " ***** ",
+                " ***** "));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *"));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      "));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                " ***** "));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "      *"));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *"));
+
+        System.out.println(String.join(" ",
+                " ***** ",
+                " ***** ",
+                " ***** ",
+                " ***** "));
+    }
+}
